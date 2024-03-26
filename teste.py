62d4cb942ba7289534a71ae52c8ba6472259a106
Michelle Canoas <163908232+micanoas@users.noreply.github.com>
2024-03-26 19:58:53 -0300

    Add files via upload

---
+++ teste.py
@@ -0,0 +1,84 @@
+#a = int(input("ano de nascimento:"))
+#b = int(input("qual ano atual:"))
+#if ((b-a)>=18):
+#    print("qual seu titulo de eleitor")
+#else:
+#    print("qual documento do responsavel")
+
+
+#******************************************
+#FUNÇÃO RECURSIVA
+# PARA RETORNAR SE CPF É VALIDO OU NÃO NO SITE
+
+#def soma():
+ #   txt = "Imprimindo resultado com variavel"
+ #    return txt
+
+#print(soma())
+
+#*******
+
+#def soma (a, b, d):
+ #   c = a + b + d
+  #  if c % 2 == 0:
+   #     return "Par"
+    #else:
+     #   return "Ímpar"
+
+
+#print(soma(4,2,9))
+
+#******************************************************
+#VETORES
+
+#while a <= 10;
+#    print("Teste")
+#    a = a + 1
+
+
+#******************************************************
+#LAÇOS E LISTA EXPANSIVA
+
+#a = ["Michelle", "Julia", "Paulo"]
+
+#for a in range(5):
+#    print("Michelle")
+
+#a = []
+#b = 1
+#while b <= 3:
+#    a.append(input("Digite o nome do funcionario: "))
+#    b = b + 1
+#print(a)
+
+#******************************************************
+#INPUT/OUTPUT
+
+#a = ["Michelle", "Bruna"]
+#a.insert(1, "Hellen")
+#print(a)
+#a.remove("Michelle")
+#print(a)
+
+
+#*******************************************************
+#TUPLA
+
+#tupla = (1,2,3,4,5,6,7,8,9)
+#if 12 in tupla:
+#    print("existe")
+#else:
+#    print("nao existe")
+
+#open("novoarquivo.txt", "x")
+
+#conteudo = open("novoarquivo.txt", "a")
+#conteudo.write("Uma linha qualquer\n")
+#conteudo.write("Segunda linha\n")
+
+#**************************************************
+
+
+
+
+
